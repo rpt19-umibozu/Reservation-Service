fd49989993687f6e9ba94907152714b79d7c9ceb
Yingwen <inwen.chen@gmail.com>
2020-04-10 10:10:56 -0600

    adding pm2 config file

---
+++ myapp.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name        : "myapp-api",
+    script      : "nodemon --inspect server/index.js",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+     "NODE_ENV": "production"
+    }
+  }
