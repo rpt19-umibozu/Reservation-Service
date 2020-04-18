77a009e78092540ddd170c663f009990e50d4a2e
Yingwen <inwen.chen@gmail.com>
2020-04-18 12:38:18 -0600

    improving on page speed

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const { connection, getListingInfo, getBookedDates } = require ('../database');
 const fs = require('fs');
 const fullPath = '/home/ubuntu/FEC_Yingwen_service/client/dist/index.html';
 var expressStaticGzip = require('express-static-gzip');
+var compression = require('compression');
 
 const app = express();
 app.use(morgan('dev'));
@@ -16,10 +17,11 @@ app.use(bodyParser.json());
 
 
 // app.use(express.static(__dirname + '/../client/dist'));
-
+app.use(compression());
 app.use('/', expressStaticGzip(path.join(__dirname + '/../client/dist'), {
   enableBrotli: true
  }));
+
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
