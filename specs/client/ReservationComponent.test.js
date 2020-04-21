e9f317d3f0e45c354b461d7d1a7f67a2ac6af555
Yingwen <inwen.chen@gmail.com>
2020-04-21 07:02:18 -0600

    aditional tests

--- specs/client/ReservationComponent.test.js
+++ specs/client/ReservationComponent.test.js
@@ -4,10 +4,12 @@ import 'babel-polyfill';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, mount, render } from 'enzyme';
 import  Reservation  from '../../client/src/index.jsx';
+import  PriceBreakup  from '../../client/src/PriceBreakup.jsx';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 jest.mock('../../client/src/index.jsx', () => 'Reservation');
+jest.mock('../../client/src/PriceBreakup.jsx', () => 'PriceBreakup');
 
 
 
@@ -28,5 +30,16 @@ describe('Reservation', () => {
   it('should contain a CalendarBoard child component', () => {
     expect(wrapper.find(Reservation)).toHaveLength(1);
   });
+
+  it('should contain text in the first node', () => {
+
+   // const div = wrapper.find('underReserve');
+    expect(wrapper.find('p.span.perNight')).toEqual({})
+
+  });
+  it('PriceBreakup component should exist', () => {
+   const priceBreakup = wrapper.find(PriceBreakup);
+   expect(priceBreakup.exists()).toBe(false)
+  })
 
 })
